3e91217d578288cbc99adb7c45f8fbe56197a892
rathna1099 <rathnaprakashkdm@gmail.com>
2024-02-28 08:13:21 +0530

    New update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+// import data from './data';
+import {useState,useRef,useEffect} from 'react';
+
+import './index.css';
+
+let url='https://course-api.com/react-tabs-project'
+
 
 function App() {
+  let [data,setData]=useState([])
+  let [loading,setLoading]=useState(true)
+  let fetchData=async ()=>{
+    let response=await fetch(url)
+    let data1=await response.json()
+    setData(data1)
+    setLoading(false)
+  }
+  useEffect(()=>{fetchData()},[])
+  let [s,setS]=useState(0);
+  let inputIndex=useRef(0)
+  console.log(inputIndex.current)
+  let clickHandler=(e)=>{
+    e.preventDefault()
+    setS(data.length-inputIndex.current)
+  }
+
+  if(loading){return <div>Loading...</div>}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='section'>
+      <div className='jobs-center'>
+      <nav className='btn-container'>
+        {data.map(i=><button className='job-btn' onClick={clickHandler} value={i.order} onClickCapture={()=>{inputIndex.current=i.order}} key={i.order}>{i.company}</button>)}
+      </nav>
+      <div className='job-info'><h3>{data[s].title}</h3>
+      <h4>{data[s].company}</h4>
+      <p>{data[s].dates}</p>
+      <ul>
+        {data[s].duties.map((j)=><li className='job-desc' key={j}>{j}</li>)}
+      </ul>
+      </div>
+      </div>
     </div>
   );
 }
